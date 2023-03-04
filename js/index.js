818c0ecaf52187a29ecb9e64781c7dd685e869f9
Austin Mazur <mazuraustin1@gmail.com>
2023-03-04 16:38:04 +0100

    Make obstacles, make game over. need to fix the car movement, and the collision

--- js/index.js
+++ js/index.js
@@ -10,40 +10,45 @@ class Car {
     this.ctx = ctx;
     this.img = new Image();
     this.img.src = image;
+    this.speed = 0;
   }
 
   draw() {
     this.ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
   }
   moveRight() {
-    this.x += 1;
+    const maxRight = canvas.width - 110;
+    this.x = Math.min(this.x + 1, maxRight);
+    this.speed = 0;
   }
 
   moveLeft() {
-    this.x -= 1;
+    const maxLeft = 60;
+    this.x = Math.max(this.x - 1, maxLeft);
+    this.speed = 0;
   }
 }
 
 class Obstacle {
-  constructor(ctx, y) {
-    this.x = window.innerWidth;
-    this.y = y;
+  constructor(ctx) {
+    this.x = Math.random() * canvas.width;
+    this.y = 0;
     this.width = 200;
-    this.height = 20
-    this.ctx = ctx
+    this.height = 20;
+    this.ctx = ctx;
+    this.color = "red";
   }
   draw() {
-    this.ctx.fillRect(this.x, this.y, this.width, this.height)
+    this.ctx.fillRect(this.x, this.y, this.width, this.height);
   }
 
   moveDown() {
-    this.y -= 1;
+    this.y += 1;
   }
 }
 
-
-
-const car = new Car(ctx, "./images/car.png");console.log(car);
+const car = new Car(ctx, "./images/car.png");
+console.log(car);
 const obstacles = [];
 
 window.onload = () => {
@@ -65,55 +70,65 @@ window.onload = () => {
   let counter = 0;
 
   function startGame() {
-    console.log("game started");
-    clearCanv();
-    
-    setInterval(() => {
+    intervalId = setInterval(() => {
       clearCanv();
-      counter++;
 
-     ctx.fillStyle = "green";
-     ctx.fillRect(0, 0, canvas.width, canvas.height);
-     //grey ontop
-     ctx.fillStyle = "grey";
-     ctx.fillRect(50, 0, 400, canvas.height);
-     //the two main white lines
-     ctx.fillStyle = "white";
-     ctx.fillRect(60, 0, 12, canvas.height);
-     ctx.fillRect(430, 0, 12, canvas.height);
-     // the middle white lines
-     ctx.fillRect(250, 0, 12, canvas.height / 20);
-     ctx.fillRect(250, 60, 12, canvas.height / 20);
-     ctx.fillRect(250, 60 * 2, 12, canvas.height / 20);
-     ctx.fillRect(250, 60 * 3, 12, canvas.height / 20);
-     ctx.fillRect(250, 60 * 4, 12, canvas.height / 20);
-     ctx.fillRect(250, 60 * 5, 12, canvas.height / 20);
-     ctx.fillRect(250, 60 * 6, 12, canvas.height / 20);
-     ctx.fillRect(250, 60 * 7, 12, canvas.height / 20);
-     ctx.fillRect(250, 60 * 8, 12, canvas.height / 20);
-     ctx.fillRect(250, 60 * 9, 12, canvas.height / 20);
-     ctx.fillRect(250, 60 * 10, 12, canvas.height / 20);
-     ctx.fillRect(250, 60 * 11, 12, canvas.height / 20);
-     ctx.fillRect(250, 60 * 12, 12, canvas.height / 20);
+      ctx.fillStyle = "green";
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+      //grey ontop
+      ctx.fillStyle = "grey";
+      ctx.fillRect(50, 0, 400, canvas.height);
+      //the two main white lines
+      ctx.fillStyle = "white";
+      ctx.fillRect(60, 0, 12, canvas.height);
+      ctx.fillRect(430, 0, 12, canvas.height);
+      // the middle white lines
+      ctx.fillRect(250, 0, 12, canvas.height / 20);
+      ctx.fillRect(250, 60, 12, canvas.height / 20);
+      ctx.fillRect(250, 60 * 2, 12, canvas.height / 20);
+      ctx.fillRect(250, 60 * 3, 12, canvas.height / 20);
+      ctx.fillRect(250, 60 * 4, 12, canvas.height / 20);
+      ctx.fillRect(250, 60 * 5, 12, canvas.height / 20);
+      ctx.fillRect(250, 60 * 6, 12, canvas.height / 20);
+      ctx.fillRect(250, 60 * 7, 12, canvas.height / 20);
+      ctx.fillRect(250, 60 * 8, 12, canvas.height / 20);
+      ctx.fillRect(250, 60 * 9, 12, canvas.height / 20);
+      ctx.fillRect(250, 60 * 10, 12, canvas.height / 20);
+      ctx.fillRect(250, 60 * 11, 12, canvas.height / 20);
+      ctx.fillRect(250, 60 * 12, 12, canvas.height / 20);
+
       
-      arrowKeys();
+      counter++;
       car.draw();
-      if(counter % 1 === 0) {
-        const obst = new Obstacle(ctx, 80);
-        obstacles.push(obst)
+      arrowKeys();
+
+      if (counter % 1000 === 0) {
+        const obst = new Obstacle(ctx);
+        obstacles.push(obst);
       }
 
       obstacles.forEach((el) => {
         el.draw();
         el.moveDown();
-        if(el.y > canvas.height) {
+        if (el.y > canvas.height) {
           obstacles.shift();
         }
-      })
-    }, 1000 / 6);
-    
-     //background green
-     
+        if (el.x < car.x + car.width && 
+          el.x + el.width > car.x &&
+          el.y < car.y + car.height &&
+          el.height + el.y > car.y) {
+            console.log("accident");
+            endGame();
+          }
+      });
+
+
+
+      // console.log(counter);
+      // console.log(speed);
+    }, 1000 / 65);
+
+    //background green
   }
 };
 
@@ -124,8 +139,9 @@ function arrowKeys() {
         console.log("up");
         break;
       case "ArrowLeft":
-        console.log("left");
-        car.moveLeft(); 
+        // console.log("left");
+        car.moveLeft();
+        counter = 0;
         break;
       case "ArrowRight":
         car.moveRight();
@@ -134,15 +150,12 @@ function arrowKeys() {
   });
 }
 
-// function drawObstacles () {
-//   obstacles.forEach((obstacle) => {
-//     ctx.fillStyle = obstacle.color;
-//     ctx.fillRect(obstacle.x, obstacle.y, obstacle.width, obstacle.height);
-//     obstacle.y += 1;
-//   })
-// }
-
-
-function clearCanv (){
+function clearCanv() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function endGame() {
+  clearInterval(intervalId);
+  clearCanv();
+  ctx.fillText("Game Over", 10, 50);
 }
